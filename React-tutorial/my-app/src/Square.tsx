b3c5982818165a91e37be02ec0d07804de1a50d5
nishikawa <Ryosuke.Nishikawa@jp.ricoh.com>
2020-10-14 15:57:03 +0900

    Reactチュートリアル ゲームを完成させる.関数コンポーネント 実施
    ※「イミュータビリティはなぜ重要なのか」はコード変更なし

--- React-tutorial/my-app/src/Square.tsx
+++ React-tutorial/my-app/src/Square.tsx
@@ -4,29 +4,17 @@ import './index.css';
 
 interface SquareProps {
   value: string;
-  onClick: Function;
+  onClick: () => void;
 }
 
 interface SquareState {
   value: string;
 }
 
-export class Square extends React.Component<SquareProps, SquareState> {
-  constructor(props: SquareProps) {
-    super(props);
-    this.state = {
-      value: '',
-    };
-  }
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-         {this.props.value}
-      </button>
-    );
-  }
+export function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
